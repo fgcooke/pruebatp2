cc08d75b9ecaf4b7c0a50fc03fcc17c2dfde13e0
Federico <72287728+fgcooke@users.noreply.github.com>
2021-07-22 23:15:50 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Listadocateg from './components/CategoriasListado';
+
+import 'bootswatch/dist/cerulean/bootstrap.min.css'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Listadocateg />
+    </div>
+  );
+}
+
+export default App;
+
